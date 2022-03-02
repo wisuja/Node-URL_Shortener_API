ddced86aa793a02b19b4473d00b1afcfa1bf4ad5
William Surya Jaya <williamsj26.wsj@gmail.com>
2022-03-02 15:43:30 +0700

    Update env variable name

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 
 const http = require('http');
-const mongoose = require('mongoose');
 const app = require('./src/app');
 
 const server = http.createServer(app);
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, function () {
   console.log('Server has started on port ' + PORT);
