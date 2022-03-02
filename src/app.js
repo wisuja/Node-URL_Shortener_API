de0c2f73dad79568beb1e7f9410fc4c4293ed7c7
William Surya Jaya <williamsj26.wsj@gmail.com>
2022-03-02 16:33:07 +0700

    Update cors

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const routes = require('./routes/url_pair');
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
 app.use(express.json());
 app.use(
@@ -21,6 +11,8 @@ app.use(
     extended: false,
   })
 );
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   return res.send('URL Shortener API is live!');
